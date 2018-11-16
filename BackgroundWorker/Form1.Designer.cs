d43216761a9e6e332874404db90524a481c95b4b
charliesands <cwsands3@gmail.com>
2018-11-16 16:58:05 -0700

    form setup

--- BackgroundWorker/Form1.Designer.cs
+++ BackgroundWorker/Form1.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.useBackgroundWorkerCheckbox = new System.Windows.Forms.CheckBox();
+            this.goButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // useBackgroundWorkerCheckbox
+            // 
+            this.useBackgroundWorkerCheckbox.AutoSize = true;
+            this.useBackgroundWorkerCheckbox.Location = new System.Drawing.Point(13, 13);
+            this.useBackgroundWorkerCheckbox.Name = "useBackgroundWorkerCheckbox";
+            this.useBackgroundWorkerCheckbox.Size = new System.Drawing.Size(141, 17);
+            this.useBackgroundWorkerCheckbox.TabIndex = 0;
+            this.useBackgroundWorkerCheckbox.Text = "Use BackgroundWorker";
+            this.useBackgroundWorkerCheckbox.UseVisualStyleBackColor = true;
+            // 
+            // goButton
+            // 
+            this.goButton.Location = new System.Drawing.Point(13, 37);
+            this.goButton.Name = "goButton";
+            this.goButton.Size = new System.Drawing.Size(75, 23);
+            this.goButton.TabIndex = 1;
+            this.goButton.Text = "Go!";
+            this.goButton.UseVisualStyleBackColor = true;
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(95, 37);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 2;
+            this.cancelButton.Text = "Cancel";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 67);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(318, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(363, 106);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.goButton);
+            this.Controls.Add(this.useBackgroundWorkerCheckbox);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BackgroundWorker example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.CheckBox useBackgroundWorkerCheckbox;
+        private System.Windows.Forms.Button goButton;
+        private System.Windows.Forms.Button cancelButton;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
